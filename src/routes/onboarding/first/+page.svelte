cfe66a823cea2558fd89e1adab45117d794203d0
Kevin Garcia <kevingarciah58@gmail.com>
2024-11-12 12:25:41 -0600

    feat: select and combobox

--- src/routes/onboarding/first/+page.svelte
+++ src/routes/onboarding/first/+page.svelte
@@ -10,6 +10,71 @@
 			isLoading = false;
 		}, 3000);
 	}
+
+	// Select
+	import * as Select from '$lib/shared/ui/select/index.js';
+
+	const fruits = [
+		{ value: 'apple', label: 'Apple' },
+		{ value: 'banana', label: 'Banana' },
+		{ value: 'blueberry', label: 'Blueberry' },
+		{ value: 'grapes', label: 'Grapes' },
+		{ value: 'pineapple', label: 'Pineapple' }
+	];
+
+	let value = $state('');
+
+	const triggerContent = $derived(fruits.find((f) => f.value === value)?.label ?? 'Select a fruit');
+
+	// ComboBox
+	import Check from 'lucide-svelte/icons/check';
+	import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
+	import { tick } from 'svelte';
+	import * as Command from '$lib/shared/ui/command/index.js';
+	import * as Popover from '$lib/shared/ui/popover/index.js';
+	import { Button } from '$lib/shared/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+
+	const frameworks = [
+		{
+			frameworkValue: 'sveltekit',
+			label: 'SvelteKit'
+		},
+		{
+			frameworkValue: 'next.js',
+			label: 'Next.js'
+		},
+		{
+			frameworkValue: 'nuxt.js',
+			label: 'Nuxt.js'
+		},
+		{
+			frameworkValue: 'remix',
+			label: 'Remix'
+		},
+		{
+			frameworkValue: 'astro',
+			label: 'Astro'
+		}
+	];
+
+	let open = $state(false);
+	let frameworkValue = $state('');
+	let triggerRef = $state<HTMLButtonElement>(null!);
+
+	const selectedValue = $derived(
+		frameworks.find((f) => f.frameworkValue === frameworkValue)?.label
+	);
+
+	// We want to refocus the trigger button when the user selects
+	// an item from the list so users can continue navigating the
+	// rest of the form with the keyboard.
+	function closeAndFocusTrigger() {
+		open = false;
+		tick().then(() => {
+			triggerRef.focus();
+		});
+	}
 </script>
 
 <div class="flex h-full w-full flex-col items-start justify-center px-[152px]">
@@ -48,27 +113,75 @@
 		<div class="flex gap-5">
 			<div class="w-1/2 space-y-2">
 				<Label class="text-lg font-semibold text-black" for="last-name">País de Residencia</Label>
-				<Input
-					class="rounded-lg border-alineados_gray-100  bg-alineados_gray-50 placeholder:text-alineados_gray-500"
-					id="last-name"
-					placeholder="Ej: Penados"
-					type="last-name"
-					autocapitalize="none"
-					autocorrect="off"
-					disabled={isLoading}
-				/>
+
+				<Select.Root type="single" name="favoriteFruit" bind:value>
+					<Select.Trigger
+						class="w-full border-alineados_gray-100 bg-alineados_gray-50 text-alineados_gray-900 focus:outline-none focus:ring-2 focus:ring-alineados_gray-100 data-[placeholder]:text-alineados_gray-500"
+					>
+						{triggerContent}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group class="bg-alineados_gray-100">
+							<Select.GroupHeading>Fruits</Select.GroupHeading>
+							{#each fruits as fruit}
+								<Select.Item
+									class="default-text bg-alineados_gray-50"
+									value={fruit.value}
+									label={fruit.label}
+								>
+									{fruit.label}
+								</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
 			</div>
 			<div class="w-1/2 space-y-2">
 				<Label class="text-lg font-semibold text-black" for="last-name">País de nacimiento</Label>
-				<Input
-					class="rounded-lg border-alineados_gray-100  bg-alineados_gray-50 placeholder:text-alineados_gray-500"
-					id="last-name"
-					placeholder="Ej: Penados"
-					type="last-name"
-					autocapitalize="none"
-					autocorrect="off"
-					disabled={isLoading}
-				/>
+
+				<Popover.Root bind:open>
+					<Popover.Trigger bind:ref={triggerRef}>
+						{#snippet child({ props })}
+							<Button
+								variant="outline"
+								class="w-full justify-between border-alineados_gray-100 bg-alineados_gray-50 text-alineados_gray-500"
+								{...props}
+								role="combobox"
+								aria-expanded={open}
+							>
+								{selectedValue || 'Select a framework...'}
+								<ChevronsUpDown class="ml-2 size-4 shrink-0 opacity-50" />
+							</Button>
+						{/snippet}
+					</Popover.Trigger>
+					<Popover.Content class="w-full">
+						<Command.Root>
+							<Command.Input placeholder="Search framework..." />
+							<Command.List>
+								<Command.Empty>No framework found.</Command.Empty>
+								<Command.Group>
+									{#each frameworks as framework}
+										<Command.Item
+											value={framework.frameworkValue}
+											onSelect={() => {
+												frameworkValue = framework.frameworkValue;
+												closeAndFocusTrigger();
+											}}
+										>
+											<Check
+												class={cn(
+													'mr-2 size-4',
+													value !== framework.frameworkValue && 'text-transparent'
+												)}
+											/>
+											{framework.label}
+										</Command.Item>
+									{/each}
+								</Command.Group>
+							</Command.List>
+						</Command.Root>
+					</Popover.Content>
+				</Popover.Root>
 			</div>
 		</div>
 		<div class="flex gap-5">
@@ -130,3 +243,9 @@
 		>
 	</div>
 </div>
+
+<style>
+	:global(.popover-content) {
+		width: 100%;
+	}
+</style>
