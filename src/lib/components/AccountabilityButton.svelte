441f8bc4b0fcfda95c2a0cee8165ef3ad981e901
Kevin Garcia <kevingarciah58@gmail.com>
2024-11-16 10:45:54 -0600

    chore: animated

--- src/lib/components/AccountabilityButton.svelte
+++ src/lib/components/AccountabilityButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import CheckCircle from '$lib/icons/CheckCircle.svelte';
+	import CircleCheck from '$lib/icons/CircleCheck.svelte';
 </script>
 
 <button
 	class="group flex items-center gap-[6px] rounded-lg bg-[#0F172A] px-6 py-4 text-white transition duration-300 ease-in-out hover:bg-[#1A2642] hover:shadow-lg hover:shadow-[#0F172A]/30"
 	aria-label="Rendir Cuentas"
 >
-	<CheckCircle width={20} height={20} />
+	<CircleCheck width={20} height={20} />
 	<p class="text-base font-medium">Rendir cuentas</p>
 </button>
