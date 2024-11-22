c9e06ee6e10a7936681fa4a986b0f413f7c843ad
Kevin Garcia <kevingarciah58@gmail.com>
2024-11-22 16:08:09 -0600

    feat: add AccountabilityBody and AccountabilityHeader components; implement layout for accountability dashboard with sticky elements and improved structure

---
+++ src/routes/dashboard/problems/accountability/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import AccountabilityBody from '$lib/modules/dashboard/Problems/AccountabilityBody.svelte';
+	import AccountabilityHeader from '$lib/modules/dashboard/Problems/AccountabilityHeader.svelte';
+	import ProblemsFilter from '$lib/modules/dashboard/Problems/ProblemsFilter.svelte';
+	import ViewProblem from '$lib/modules/dashboard/Problems/ViewProblem.svelte';
+</script>
+
+<div class="sticky top-0 z-10 bg-white">
+	<AccountabilityHeader />
+</div>
+
+<div class="flex w-full items-start pb-10">
+	<div class=" flex w-7/12 flex-col">
+		<div class="sticky top-32 z-10 bg-white">
+			<ProblemsFilter />
+		</div>
+
+		<div class="pl-10">
+			<AccountabilityBody />
+		</div>
+	</div>
+
+	<div class="sticky top-32 z-10 flex w-5/12 justify-center bg-white">
+		<ViewProblem />
+	</div>
+</div>
