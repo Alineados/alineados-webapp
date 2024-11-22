3b097354eb09a755258baf6de47e1f3e21f041bd
Kevin Garcia <kevingarciah58@gmail.com>
2024-11-22 15:03:31 -0600

    feat: make headers sticky in dashboard problem views; enhance layout for better accessibility and user experience

--- src/routes/dashboard/problems/edit/+page.svelte
+++ src/routes/dashboard/problems/edit/+page.svelte
@@ -5,15 +5,22 @@
 	import ViewProblem from '$lib/modules/dashboard/Problems/ViewProblem.svelte';
 </script>
 
-<ProblemHeader />
+<div class="sticky top-0 z-10 bg-white">
+	<ProblemHeader />
+</div>
 
 <div class="flex w-full items-start pb-10">
-	<div class="flex w-7/12 flex-col">
-		<ProblemsFilter />
-		<ProblemBody />
+	<div class=" flex w-7/12 flex-col">
+		<div class="sticky top-32 z-10 bg-white">
+			<ProblemsFilter />
+		</div>
+
+		<div class="pl-10">
+			<ProblemBody />
+		</div>
 	</div>
 
-	<div class="flex w-5/12 justify-center">
+	<div class="sticky top-32 z-10 flex w-5/12 justify-center bg-white">
 		<ViewProblem />
 	</div>
 </div>
