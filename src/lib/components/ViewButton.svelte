a776c56f95af641fc1d1ba1c6f183d9e8797e1d7
Kevin Garcia <kevingarciah58@gmail.com>
2024-12-16 16:30:51 -0600

    Add DateDialog and DatePicker components; remove unused ViewButton functionality

--- src/lib/components/ViewButton.svelte
+++ src/lib/components/ViewButton.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import Eye from '$lib/icons/Eye.svelte';
-	import { goto } from '$app/navigation';
-
-	function handleClick() {
-		goto('/dashboard/problems/view');
-	}
 </script>
 
 <button
@@ -14,7 +9,6 @@
 	}}
 	class="group flex items-center gap-[6px] rounded-lg bg-alineados-blue-900 px-5 py-4 text-white transition duration-300 ease-in-out hover:bg-alineados-blue-800 hover:shadow-lg hover:shadow-alineados-blue-900/30"
 	aria-label="Ver"
-	on:click={handleClick}
 >
 	<Eye styleTw="size-4" />
 	<p class="text-sm font-medium">Ver</p>
