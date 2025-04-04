c284ccbfe0987869b5953fa6c4c426cfbdcdae65
LucaBia <gian.luca.99@hotmail.com>
2025-04-03 18:43:48 -0600

    feat: exportar pdf

--- src/lib/components/MoreButton.svelte
+++ src/lib/components/MoreButton.svelte
@@ -3,7 +3,10 @@
 	import ThreeDots from '$lib/icons/ElipsisVertical.svelte';
 	import Blocked from '$lib/icons/Blocked.svelte';
 	import Lock from '$lib/icons/Lock.svelte';
+	import File from '$lib/icons/File.svelte';
 	import { updateSecurityAndActive, problemCard } from '$lib/stores';
+    import html2canvas from 'html2canvas';
+    import { jsPDF } from 'jspdf';
 
 	// Function to update
 	// 1. active / inactive .....  2. security / non-security
@@ -12,6 +15,21 @@
 			updateSecurityAndActive(kind);
 		}, 500);
 	}
+
+    async function exportToPDF() {
+        const element = document.body; // or any specific element you want to capture
+        const canvas = await html2canvas(element);
+        const imgData = canvas.toDataURL('image/png');
+        
+        const pdf = new jsPDF({
+            orientation: 'landscape',
+            unit: 'px',
+            format: [canvas.width, canvas.height]
+        });
+        
+        pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+        pdf.save('alineados-export.pdf');
+    }
 </script>
 
 <DropdownMenu.Root>
@@ -51,6 +69,13 @@
 				{/if}
 			</span>
 		</DropdownMenu.Item>
+		<DropdownMenu.Item
+            onclick={exportToPDF}
+            class="bg-white hover:cursor-pointer hover:bg-alineados-gray-100"
+        >
+            <File class="mt-0.5" />
+            <span class="font-normal">Exportar</span>
+        </DropdownMenu.Item>
 		<!--Content group -->
 		<!-- <DropdownMenu.Group>
 			<DropdownMenu.Sub>
