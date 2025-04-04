0fe6a1dc8e1002613da3729cced5f525ea565e97
LucaBia <gian.luca.99@hotmail.com>
2025-04-03 19:13:55 -0600

    fix: template mejorado

--- src/lib/modules/dashboard/Problems/PillarHeader.svelte
+++ src/lib/modules/dashboard/Problems/PillarHeader.svelte
@@ -2,6 +2,7 @@
 	import CardFilter from '$lib/components/CardFilter.svelte';
 	import NewProblemButton from '$lib/components/NewProblemButton.svelte';
 	import ViewButton from '$lib/components/ViewButton.svelte';
+	import MoreButtonProblems from '$lib/components/MoreButtonProblems.svelte';
 	import { filterBy } from '$lib/stores';
 
 	// Filter items object state
@@ -32,30 +33,9 @@
 		<p class="text-5xl font-bold text-alineados-blue-900">Situaciones por Mejorar</p>
 
 		<div class="flex flex-row gap-4 self-start">
-			<!-- <a
-				href="https://forms.office.com/r/huDFV67VZB"
-				target="_blank"
-				class="flex flex-row items-center gap-2 rounded-lg border bg-alineados-orange-800 px-4 py-3 text-sm text-white transition duration-300 ease-in-out hover:shadow-lg"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="size-5"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-					/>
-				</svg>
-
-				Reportar incidentes
-			</a> -->
 			<NewProblemButton />
 			<ViewButton />
+			<MoreButtonProblems />
 		</div>
 	</div>
 </div>
