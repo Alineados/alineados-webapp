50672943cc7de2dc4078f638ff8933f7bdd5eb05
LucaBia <gian.luca.99@hotmail.com>
2025-04-05 12:04:33 -0600

    fix: styles login

--- src/lib/modules/auth/components/AsideContent.svelte
+++ src/lib/modules/auth/components/AsideContent.svelte
@@ -2,7 +2,7 @@
 	import AsideImage from '$lib/icons/AsideImage.svelte';
 </script>
 
-<div class="flex h-full flex-col items-center">
+<div class="flex h-full flex-col items-center justify-center">
 	<div class="flex-column items-center justify-center">
 		<!-- <AsideImage size={1.3} /> -->
 		<img
@@ -10,7 +10,7 @@
 			alt="Alineados Logo"
 			class="h-[30rem] w-auto"
 		/>
-		<h1 class="text-5xl text-white">Vive tu propósito</h1>
+		<h1 class="text-5xl text-white text-center">Vive tu propósito</h1>
 	</div>
 	<!-- <div class="mb-10 flex flex-col items-center justify-center gap-4"> -->
 		<!-- <p class="w-10/12 text-center text-xs font-medium text-[#8194C2]">
