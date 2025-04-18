35f6068d130da3074fa4ebbc4b05d40349feadfb
LucaBia <gian.luca.99@hotmail.com>
2025-04-18 12:53:48 -0600

    feat: color titulos ver SPM y links

--- src/lib/modules/dashboard/Problems/ViewTableHeader.svelte
+++ src/lib/modules/dashboard/Problems/ViewTableHeader.svelte
@@ -12,13 +12,13 @@
 	class="top-0 grid rounded-t-xl border border-alineados-gray-100 bg-alineados-gray-50"
 	style="grid-template-columns: 2fr 3fr 3fr;"
 >
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-gray-700">
+	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
 		Nombre Identificativo
 	</p>
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-gray-700">
+	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
 		{firstCriterionLabel}
 	</p>
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-gray-700">
+	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
 		{secondCriterionLabel}
 	</p>
 </div>
