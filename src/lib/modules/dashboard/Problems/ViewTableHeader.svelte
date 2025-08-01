aebf6de6c8f5e4fd4a394daa6b71d22abdbf3f32
LucaBia <gian.luca.99@hotmail.com>
2025-08-01 14:23:40 +0200

    feat: header amarillo resumen

--- src/lib/modules/dashboard/Problems/ViewTableHeader.svelte
+++ src/lib/modules/dashboard/Problems/ViewTableHeader.svelte
@@ -9,16 +9,16 @@
 </script>
 
 <div
-	class="top-0 grid rounded-t-xl border border-alineados-gray-100 bg-alineados-gray-50"
+	class="top-0 grid rounded-t-xl border border-alineados-orange-900 bg-alineados-orange-600"
 	style="grid-template-columns: 2fr 3fr 3fr;"
 >
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
+	<p class="px-6 py-3 text-left text-base font-bold">
 		Nombre Identificativo
 	</p>
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
+	<p class="px-6 py-3 text-left text-base font-bold">
 		{firstCriterionLabel}
 	</p>
-	<p class="px-6 py-3 text-left text-base font-medium text-alineados-orange-900">
+	<p class="px-6 py-3 text-left text-base font-bold">
 		{secondCriterionLabel}
 	</p>
 </div>
